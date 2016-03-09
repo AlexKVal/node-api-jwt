fd7b415a01c5267e1ab736b407d2a4aff6d280b5
AlexKVal <alexkval@gmail.com>
2016-03-09 19:14:01 +0200

    add some routes and use faker

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
+const faker = require('faker')
 
 const config = require('./config')
 const User = require('./app/models/user')
@@ -25,7 +26,6 @@ app.get('/', function(req, res) {
   res.send(`The API is at localhost:${port}/api`)
 })
 
-// API
 app.get('/setup', function(req, res) {
   const sampleUser = new User({
     name: 'Rember Johrdan',
@@ -41,5 +41,23 @@ app.get('/setup', function(req, res) {
   })
 })
 
+// API
+const api = express.Router()
+
+// POST http://localhost:8080/api/authenticate
+
+// verify a token
+
+// GET http://localhost:8080/api/
+api.get('/', function(req, res) {
+  res.json({ message: faker.company.catchPhrase() })
+})
+
+// GET http://localhost:8080/api/users
+api.get('/users', function(req, res) {
+  User.find({}, (err, users) => res.json(users))
+})
+
+app.use('/api', api)
 
 app.listen(port, () => console.log(`The server listens on port ${port}`))
